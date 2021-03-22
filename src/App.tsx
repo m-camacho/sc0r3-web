59e578995363fdcee610af0749ac4648758d1aea
m-camacho <mario.camacho2@ymail.com>
2021-03-22 16:55:44 -0400

    Very basic AddPlayer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 
 import React from "react";
+import AddPlayer from "./components/AddPlayer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to sc0r3!</p>
+        <AddPlayer />
       </header>
     </div>
   );
