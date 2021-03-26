0f87285a667bf765cfcbe37ca9ad75323005ae84
m-camacho <mario.camacho2@ymail.com>
2021-03-26 16:05:24 -0400

    AddPlayer better experience

--- src/components/AddPlayer.tsx
+++ src/components/AddPlayer.tsx
@@ -1,10 +1,21 @@
 import "./AddPlayer.scss";
 
 import React, { useState } from "react";
+import { KEYCODE } from "./constants";
 
 function AddPlayer() {
   const [inputValue, setInputValue] = useState("");
   const [players, setPlayers] = useState<string[]>([]);
+
+  const handleKeyUp = (event: any) => {
+    if (event && event.keyCode === KEYCODE.ENTER) {
+      setPlayers([...players, inputValue]);
+      setInputValue("");
+    }
+    if (event && event.keyCode === KEYCODE.ESCAPE) {
+      setInputValue("");
+    }
+  };
 
   return (
     <div className="mc-add-player">
@@ -14,10 +25,12 @@ function AddPlayer() {
           type="text"
           value={inputValue}
           onChange={({ target }) => setInputValue(target.value)}
+          onKeyUp={handleKeyUp}
         />
         <button
           onClick={() => {
             setPlayers([...players, inputValue]);
+            setInputValue("");
           }}
         >
           Add
@@ -30,6 +43,7 @@ function AddPlayer() {
       )}
       <div>
         <button
+          className="start-game-button"
           onClick={() => {
             console.log(players);
           }}
---
+++ src/components/constants.ts
@@ -0,0 +1,8 @@
+export const KEYCODE = {
+  TAB: 9,
+  ENTER: 13,
+  ESCAPE: 27,
+  SPACE_BAR: 32,
+  ARROW_UP: 38,
+  ARROW_DOWN: 40,
+};
